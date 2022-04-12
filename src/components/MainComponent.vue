c1d3fe33a696569301ffbb3dbb60d3783c88536a
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2022-04-12 18:30:59 +0200

    DEL-3: Add new divs

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,3 +1,13 @@
 <template>
   <div>this is main component</div>
+  <div>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem harum
+    reiciendis quia repudiandae maxime voluptate delectus vero, excepturi magnam
+    atque.
+  </div>
+  <div>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem harum
+    reiciendis quia repudiandae maxime voluptate delectus vero, excepturi magnam
+    atque.
+  </div>
 </template>
